addb6cde6ab6e0ace01aedc180128a42f2e8525b
0xreentrant <0x7e1e@3six9.io>
2023-01-21 21:54:23 -0700

    Updates to Listings Editor
    - added state where updates to the document will enable the "save" button
    - updating the edited doc, then reverting to the last saved changes will
      re-disable the "save" button
    - extracted the "Button" component

---
+++ src/ListingEditor/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const Button = ({to='', children, className='', disabled, onClick, ...props}) => {
+  const navigate = useNavigate()
+  const withDisabledBg = disabled ? 'bg-gray-400' : ''
+  const clickHandler = e => {
+    e.preventDefault()
+
+    if (disabled) {
+      return 
+    }
+
+    if (to) {
+      navigate()
+      return;
+    }
+
+    onClick && onClick(e)
+  }
+
+  return (
+    <a href={to} onClick={clickHandler} className={`btn ${className} ${withDisabledBg}`} {...props}>
+      {children}
+    </a>
+  )
+}
+
+
