2a058a35d889e42ed0a2e9d8d91752a2937b72c8
0xreentrant <0x7e1e@3six9.io>
2023-01-21 21:54:22 -0700

    Updates to contract side, fleshing out Resume contract

--- onchain/contracts/Resume.sol
+++ onchain/contracts/Resume.sol
@@ -1,17 +1,28 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Resume is ERC721 {
-  string public metadata;
-  string public ipfsHash;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-  constructor(
-    string memory _metadata, 
-    string memory _ipfsHash
-  ) ERC721("Tokenised Resume", "tres") {
-    metadata = _metadata;
-    ipfsHash = _ipfsHash;
+  constructor() ERC721("Tokenised Resume", "TRES") {}
+
+  /**
+   * Generate a TRES token
+   * @param to address of target wallet
+   * @param tokenURI JSON doc w/ IPFS hash included
+   */
+  function toTRES(address to, string memory tokenURI) public returns (uint256) {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mint(to, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    return newItemId;
   }
 }
