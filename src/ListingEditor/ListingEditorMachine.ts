226478176655f675e0f14f54610cccf037826950
0xreentrant <0x7e1e@3six9.io>
2023-01-21 21:54:24 -0700

    Refactor the state machine to prevent changing edit/preview views from interfering with editor and save states controls
    - UI view state and editor state are separate, parallel states
    - save also moved to own parallel state

--- src/ListingEditor/ListingEditorMachine.ts
+++ src/ListingEditor/ListingEditorMachine.ts
@@ -2,78 +2,26 @@ import {createMachine, assign} from "xstate";
 import {STATES} from '../constants'
 
 export type MachineContext = {
-  doneLoading: boolean,
-  ipfsHash: string,
-  ipfsDocument: string,
-}
+  doneLoading: boolean;
+  ipfsHash: string;
+  ipfsDocument: string;
+};
 
 export const editorMachine = createMachine<any, any>(
   {
-    initial: "initializing",
     context: {
       isNew: true,
       doneLoading: false,
       ipfsHash: "",
       ipfsDocument: "",
       buffer: "",
-      currentView: 'Edit',
+      currentView: "Edit",
     },
+    // allow the UI controls states and the editor states work in parallel, so I can simply 
+    // track state.value for editor state while in 'preview' view. Then the 'save' button 
+    // won't flip between disabled/enabled when flipping between views.  
+    type: "parallel",
     states: {
-      initializing: {
-        initial: "validating",
-        states: {
-          validating: {
-            on: {
-              IS_NEW: "#editing",
-              IS_EXISTING: [
-                {
-                  target: "fetching_ipfs_hash",
-                  cond: "withValidIPFSHash",
-                },
-                { target: "errors.hasInvalidIPFSHash", cond: "invalid" },
-              ],
-            },
-          },
-          fetching_ipfs_hash: {
-            entry: () => console.log("has hash, fetching"),
-            invoke: {
-              src: 'fetchIPFSDocument',
-              onDone: {
-                target: "#editing",
-                actions: [
-                  "setDoneLoading",
-                  assign({ ipfsDocument: (_, e) => e.data }),
-                  assign({ buffer: (_, e) => e.data }),
-                ],
-              },
-              onError: {
-                target: 'errors.networkError'
-              }
-            },
-          },
-          errors: {
-            initial: "none",
-            states: {
-              none: {},
-              hasInvalidIPFSHash: {},
-              networkError: {},
-            },
-          },
-        },
-      },
-      saving: {
-        id: "saving",
-        invoke: {
-          src: () => {
-            // TODO: flesh out save
-            return Promise.resolve("hello");
-          },
-          onDone: {
-            target: "#editing.clean",
-            actions: ['updateIPFSDocument', 'setNoLongerNew']
-          },
-        },
-      },
       editing: {
         id: "editing",
         initial: "clean",
@@ -87,54 +35,116 @@ export const editorMachine = createMachine<any, any>(
             },
           },
           dirty: {
-            entry: () => console.log("setting dirty - got updates to heading/inMemoryText"),
             on: {
               UPDATE: [
-                {actions: "updateBuffer", cond: 'isDirty'},
-                {target: 'clean', actions: 'updateBuffer'},
+                { cond: "isOriginalDoc", actions: "updateBuffer", target: "clean" },
+                { actions: "updateBuffer" },
               ],
               SAVE: {
-                target: "#saving",
-                actions: () => console.log('Saving'),
+                target: "clean",
               },
               RESET: {
                 target: "clean",
-                actions: () => console.log("resetting dirty - inMemoryText same as latestSavedText"),
               },
             },
           },
         },
-        on: {
-          TOGGLE_VIEW: {
-            target: 'previewing',
-            actions: 'setPreviewing'
-          }
-        }
       },
-      previewing: {
-        on: {
-          TOGGLE_VIEW: {
-            target: 'editing',
-            actions: 'setEditing'
-          }
-        }
-      }
+      ui: {
+        id: "ui",
+        initial: "initializing",
+        states: {
+          initializing: {
+            initial: "validating",
+            states: {
+              validating: {
+                on: {
+                  IS_NEW: "#ui.editing",
+                  IS_EXISTING: [
+                    { target: "fetching_ipfs_hash", cond: "withValidIPFSHash" },
+                    { target: "errors.hasInvalidIPFSHash", cond: "invalid" },
+                  ],
+                },
+              },
+              fetching_ipfs_hash: {
+                invoke: {
+                  src: "fetchIPFSDocument",
+                  onDone: {
+                    target: "#ui.editing",
+                    actions: [
+                      "setDoneLoading",
+                      assign({ ipfsDocument: (_, e) => e.data }),
+                      assign({ buffer: (_, e) => e.data }),
+                    ],
+                  },
+                  onError: {
+                    target: "errors.networkError",
+                  },
+                },
+              },
+              errors: {
+                initial: "none",
+                states: {
+                  none: {},
+                  hasInvalidIPFSHash: {},
+                  networkError: {},
+                },
+              },
+            },
+          },
+          editing: {
+            on: {
+              TOGGLE_VIEW: {
+                target: "previewing",
+                actions: "setPreviewing",
+              },
+            },
+          },
+          previewing: {
+            on: {
+              TOGGLE_VIEW: {
+                target: "editing",
+                actions: "setEditing",
+              },
+            },
+          },
+        },
+      },
+      save: {
+        id: "save",
+        initial: "idle",
+        states: {
+          idle: {
+            on: {
+              SAVE: "saving",
+            },
+          },
+          saving: {
+            invoke: {
+              src: "saveResume",
+              onDone: {
+                target: "idle",
+                actions: ["updateIPFSDocument", "setNoLongerNew"],
+              },
+            },
+          },
+        },
+      },
     },
   },
   {
     actions: {
-      setDoneLoading: assign({doneLoading: true}),
-      setNoLongerNew: assign({isNew: false}),
-      setPreviewing: assign({currentView: STATES.PREVIEW}),
-      setEditing: assign({currentView: STATES.EDIT}),
-      updateIPFSDocument: assign({ipfsDocument: ctx => ctx.buffer}),
-      updateBuffer: assign({buffer: (_, e) => e.value}),
+      setDoneLoading: assign({ doneLoading: true }),
+      setNoLongerNew: assign({ isNew: false }),
+      setPreviewing: assign({ currentView: STATES.PREVIEW }),
+      setEditing: assign({ currentView: STATES.EDIT }),
+      updateIPFSDocument: assign({ ipfsDocument: (ctx) => ctx.buffer }),
+      updateBuffer: assign({ buffer: (_, e) => e.value }),
     },
     guards: {
-      isDirty: (ctx, e) => e.value !== ctx.ipfsDocument,
-      withValidIPFSHash: ctx => ctx.ipfsHash.length > 0,
+      isOriginalDoc: (ctx, e) => e.value === ctx.ipfsDocument,
+      withValidIPFSHash: (ctx) => ctx.ipfsHash.length > 0,
       invalid: () => true,
     },
   }
 );
-
